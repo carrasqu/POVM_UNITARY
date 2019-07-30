fix stop gradient issue for reverse sampling

transformer/training.py
@@ -42,14 +42,14 @@ j_init = 0
 povm_='Tetra'
 povm = POVM(POVM=povm_, Number_qubits=MAX_LENGTH, initial_state='+',Jz=1.0,hx=1.0,eps=10./float(T))
 
-#mps = MPS(POVM=povm_,Number_qubits=MAX_LENGTH,MPS="Graph")
+mps = MPS(POVM=povm_,Number_qubits=MAX_LENGTH,MPS="Graph")
 bias = povm.getinitialbias("+")
 
 # define target state
-#psi = 1/2.0 * np.array([1.,1.,1.,-1], dtype=complex)
 povm.construct_psi()
 povm.construct_ham()
 psi, E = povm.ham_eigh()
+psi = 1/2.0 * np.array([1.,1.,1.,-1], dtype=complex)
 pho = np.outer(psi, np.conjugate(psi))
 prob = ncon((pho,povm.Mn),([1,2],[-1,2,1])).real
 
@@ -70,10 +70,10 @@ cFid2 = np.dot(np.sqrt(prob), np.sqrt(prob_povm))
 Fid2 = ncon((pho,pho_povm),([1,2],[2,1]))
 print(cFid2, Fid2)
 
-plt.figure(1)
-plt.bar(np.arange(4**MAX_LENGTH),prob)
-plt.figure(2)
-plt.bar(np.arange(4**MAX_LENGTH),prob_povm)
+#plt.figure(1)
+#plt.bar(np.arange(4**MAX_LENGTH),prob)
+#plt.figure(2)
+#plt.bar(np.arange(4**MAX_LENGTH),prob_povm)
 
 
 
@@ -102,9 +102,9 @@ for t in range(T):
   for j in range(j_init,MAX_LENGTH-1):
 
       sites=[j,j+1] # on which sites to apply the gate
-      #gate = povm.p_two_qubit[1] # CZ gate
+      gate = povm.p_two_qubit[1] # CZ gate
       #gate = povm.Up # imaginary time evolution
-      gate = povm.Up2 # imaginary time evolution
+      #gate = povm.Up2 # imaginary time evolution
 
       if Ndataset != 0:
           ## it ensures at least one batch size samples, since Ncall can be zero
@@ -147,18 +147,18 @@ for t in range(T):
                   loss = train_step(flip,co,gtype,batch_size,ansatz)
 
                   #samp,llpp = sample(100000) # get samples from the mode
-                  #samp,llpp = sample(ansatz,1000) # get samples from the mode
+                  samp,llpp = sample(ansatz,1000) # get samples from the mode
 
                   #np.savetxt('./samples/samplex_'+str(epoch)+'_iteration_'+str(idx)+'.txt',samp+1,fmt='%i')
                   #np.savetxt('./samples/logP_'+str(epoch)+'_iteration_'+str(idx)+'.txt',llpp)
-                  #cFid, cFidError, KL, KLError = mps.cFidelity(tf.cast(samp,dtype=tf.int64),llpp)
-                  #Fid, FidErrorr = mps.Fidelity(tf.cast(samp,dtype=tf.int64))
-                  #print('cFid: ', cFid, cFidError,Fid, FidErrorr)
+                  cFid, cFidError, KL, KLError = mps.cFidelity(tf.cast(samp,dtype=tf.int64),llpp)
+                  Fid, FidErrorr = mps.Fidelity(tf.cast(samp,dtype=tf.int64))
+                  print('cFid: ', cFid, cFidError,Fid, FidErrorr)
 
                   prob_povm = np.exp(vectorize(MAX_LENGTH, target_vocab_size, ansatz))
                   pho_povm = ncon((prob_povm,povm.Ntn),([1],[1,-1,-2]))
-                  Et = np.trace(pho_povm @ povm.ham)
-                  print('exact E:', E, 'current E:', Et.real)
+                  #Et = np.trace(pho_povm @ povm.ham)
+                  #print('exact E:', E, 'current E:', Et.real)
                   cFid2 = np.dot(np.sqrt(prob), np.sqrt(prob_povm))
                   Fid2 = ncon((pho,pho_povm),([1,2],[2,1]))
                   print('cFid2: ', cFid2, Fid2)

transformer/training2.py
@@ -42,7 +42,7 @@ j_init = 0
 povm_='Tetra'
 povm = POVM(POVM=povm_, Number_qubits=MAX_LENGTH, initial_state='+',Jz=1.0,hx=1.0,eps=10./float(T))
 
-#mps = MPS(POVM=povm_,Number_qubits=MAX_LENGTH,MPS="Graph")
+mps = MPS(POVM=povm_,Number_qubits=MAX_LENGTH,MPS="Graph")
 bias = povm.getinitialbias("+")
 
 # define target state
@@ -136,7 +136,7 @@ for t in range(T):
       counter=0
 
       samples = tf.stop_gradient(samples)
-      lP = tf.stop_gradient(lP)
+      #lP = tf.stop_gradient(lP)
 
       #ept = tf.random.shuffle(samples)
       ept = tf.random.shuffle(np.concatenate((samples,lP),axis=1))
@@ -158,27 +158,28 @@ for t in range(T):
 
 
                   #flip,co = flip2_tf(batch,gate,target_vocab_size,sites)
+                  #flip,co = flip2_tf2(batch,gate,target_vocab_size,sites)
 
                   #loss = train_step(flip,co,gtype,batch_size,ansatz)
 
                   flip,co = flip2_reverse_tf(batch[:, :2],gate,target_vocab_size,sites)
-                  loss = train_step2(flip,co,gtype,batch[:,2],ansatz)
+                  loss = train_step2(flip,co,gtype,batch,ansatz)
 
 
                   #samp,llpp = sample(100000) # get samples from the mode
-                  #samp,llpp = sample(ansatz,1000) # get samples from the mode
+                  samp,llpp = sample(ansatz,1000) # get samples from the mode
 
                   #np.savetxt('./samples/samplex_'+str(epoch)+'_iteration_'+str(idx)+'.txt',samp+1,fmt='%i')
                   #np.savetxt('./samples/logP_'+str(epoch)+'_iteration_'+str(idx)+'.txt',llpp)
-                  #cFid, cFidError, KL, KLError = mps.cFidelity(tf.cast(samp,dtype=tf.int64),llpp)
-                  #Fid, FidErrorr = mps.Fidelity(tf.cast(samp,dtype=tf.int64))
-                  #print('cFid: ', cFid, cFidError,Fid, FidErrorr)
+                  cFid, cFidError, KL, KLError = mps.cFidelity(tf.cast(samp,dtype=tf.int64),llpp)
+                  Fid, FidErrorr = mps.Fidelity(tf.cast(samp,dtype=tf.int64))
+                  print('cFid: ', cFid, cFidError,Fid, FidErrorr)
                   #Fidelity.append(np.array([cFid, Fid]))
 
                   prob_povm = np.exp(vectorize(MAX_LENGTH, target_vocab_size, ansatz))
                   pho_povm = ncon((prob_povm,povm.Ntn),([1],[1,-1,-2]))
-                  Et = np.trace(pho_povm @ povm.ham)
-                  print('exact E:', E, 'current E:', Et.real)
+                  #Et = np.trace(pho_povm @ povm.ham)
+                  #print('exact E:', E, 'current E:', Et.real)
                   cFid2 = np.dot(np.sqrt(prob), np.sqrt(prob_povm))
                   Fid2 = ncon((pho,pho_povm),([1,2],[2,1]))
                   print('cFid2: ', cFid2, Fid2)

transformer/transformer2.py
@@ -484,18 +484,22 @@ def loss_function(flip,co,gtype,batch_size,ansatz):
     return loss #, loss2
 
 
-def loss_function2(flip,co,gtype,batch_lP,ansatz):
+def loss_function2(flip,co,gtype,batch,ansatz):
 
     target_vocab_size = ansatz.decoder.target_vocab_size
-    batch_size = batch_lP.shape[0]
+    batch_size = batch.shape[0]
 
     f = tf.cond(tf.equal(gtype,1), lambda: target_vocab_size, lambda: target_vocab_size**2)
+    samples = tf.cast(batch[:,:2], dtype=tf.uint8) # c are configurations
+    batch_lP = logP(samples,ansatz, training=True)
     c = tf.cast(flip, dtype=tf.uint8) # c are configurations
-    Pj = tf.exp(logP(c,ansatz, training=True))
+    Pj = tf.exp(logP(c,ansatz))
     co_Pj = tf.reshape(co*Pj,(batch_size, f))
     co_Pj_sum = tf.reduce_sum(co_Pj, axis=1)
+    co_Pj_sum = tf.stop_gradient(co_Pj_sum)
+    batch_prob = tf.stop_gradient(tf.exp(batch[:, 2]))
 
-    loss = -tf.reduce_sum( co_Pj_sum * batch_lP / tf.exp(batch_lP)) / tf.cast(batch_size,tf.float32)
+    loss = -tf.reduce_sum( co_Pj_sum * batch_lP / batch_prob) / tf.cast(batch_size,tf.float32)
     #loss2 = -tf.reduce_mean(co * lnP) * tf.cast(f,tf.float32)
     return loss #, loss2
 